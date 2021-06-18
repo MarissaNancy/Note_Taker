aa9b63e304f383b87c3d7a2c3aa725c440375299
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-18 15:57:30 -0700

    added readme

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
-//ask about router const
+//router const like using app
 const path = require('path');
 
+// const { notes } = require('');//filename?? so we can generate notes
+
+router.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db.json"));
+});
+
+
+//for this note parameter we will need to give note an id?
+// router.post("/api/notes", function(req, res) {
--- server.js
+++ server.js
@@ -7,22 +7,15 @@ const PORT = 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-// app.use('/api',apiRoutes);
+app.use('/api',apiRoutes);
 app.use('/', htmlRoutes);
 
 // const { notes } = require('');//filename?? so we can generate notes
 
-//write note
-//delete note
+
+
+//function to delete note
 //ex 26 6.10.21
 
-//whatever comes back goes to apiroutes
-//export
-
-function newNote(body, notesTaken){
-    const notes = body;//sets notes to body
-    notesTaken.push(notes)//this will get the notesTaken and push it into the notes body
-    //write fs here??
-}
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
