2d10f0c345d7f16f7c2e37c1c16e346697c744fa
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-19 14:42:23 -0700

    created note funtciton

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,24 @@ router.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "db.json"));
 });
 
+router.get("/api/notes/:id", (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).send(" Note not found!");
+    }
+});
 
+router.post("/api/notes", (req, res) => {
+    req.body.id = notes.length.toString();
+  
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
+});
 //for this note parameter we will need to give note an id?
 // router.post("/api/notes", function(req, res) {
--- server.js
+++ server.js
@@ -10,10 +10,25 @@ app.use(express.static('public'));
 app.use('/api',apiRoutes);
 app.use('/', htmlRoutes);
 
-// const { notes } = require('');//filename?? so we can generate notes
+// const { notes } = require('./db/db.json');//filename?? so we can generate notes
 
 
 
+
+// function findnoteId(id, arrayNotes) {
+//     const result = arrayNotes.filter((note) => note.id === id)[0];
+//     return result;
+// }
+
+// function creatNote(body, arrayNotes) {
+//     const note = body;
+//     arrayNotes.push(note);
+//     fs.writeFileSync(
+//       path.join(__dirname, "./db/db.json"),
+//       JSON.stringify({ notes: notesArray }, null, 2)
+//     );
+//     return note;
+// }
 //function to delete note
 //ex 26 6.10.21
 
