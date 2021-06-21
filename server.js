cd91ecc080043128280a25d3e49cc7262b2b9b7c
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-21 15:33:05 -0700

    changed port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require ("express");
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
-const PORT = 8080;
+const PORT = 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
